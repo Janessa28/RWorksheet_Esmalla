fef2a5cf4fbecb8ad7ad83dc506f17276c76311b
Your Name <you@example.com>
2023-09-20 04:08:13 +0000

    jamjam esmalla

--- Rworksheet1.R
+++ Rworksheet1.R
@@ -3,10 +3,17 @@ age <- c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25
 length(age)
 
 reciprocal_age <- 1/age
- reciprocal_age
- 
+reciprocal_age
+
 max(age)
 
 min(age)
+
+data <-c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+
+length(data)
+
+data * 2
+
 
 
