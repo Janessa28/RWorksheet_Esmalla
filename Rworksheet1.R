9f5a206fd1251d187a30b3da8820c14c68151873
Janessa Esmalla <janessamarie.esmalla@students.isatu.edu.ph>
2023-09-26 00:50:07 +0000

    jamjam

--- Rworksheet1.R
+++ Rworksheet1.R
@@ -1,69 +1,75 @@
 #1
-age <- c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
+vector_age <- c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,49,50,37,46,25,17,37,42,53,41,51,35,24,33,41)
 
-length(age)
+length(vector_age)
 #2
-reciprocal_age <- 1/age
+reciprocal_age <- 1/vector_age
 reciprocal_age
 #3
 
-new_age <- c(age,0,age)
+new_age <- c(vector_age,0,vector_age)
+new_age
 
 #4
  sort(new_age)
 #5
- max(age)
+ max(vector_age)
 
- min(age)
+ min(vector_age)
 #6
-data <-c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+vector_data <-c(2.4,2.8,2.1,2.5,2.4,2.2,2.5,2.3,2.5,2.3,2.4,2.7)
+vector_data
 
-length(data)
 #7
-data * 2
+new_vector_data <- vector_data * 2
+new_vector_data
+
+length(vector_data)
+
 
 #8.1
-seq.int(1,100)
-length(seq.int(1,100))
+OneTo100 <-(1:100)
+OneTo100
 
 #8.2
-seq(20,60)
-length(seq(20,60))
+TwentyTo60 <-(20:60)
+TwentyTo60
 
 #8.3
-(mean(20:60))
-length(mean(20:60))
+MeanOf20To60 <- mean(TwentyTo60)
+MeanOf20To60
 
 #8.4
-(sum(51:91))
-length(sum(51:91))
+totalSum <- seq(51:91)
+totalSum <- sum(51:91)
+totalSum 
+
+OneTo1000 <- seq(1:1000)
+OneTo1000
 
 #8.5
-sum(length(seq),length(seq_integer),length(mean),length(sum))
-
+SumOfAll <-(+length(OneTo100)+length(TwentyTo60)+length(MeanOf20To60)+length(SumOf51To91))
+SumOfAll
 
 #9
-Filter(function(i) { all(i %% c(2,3,7) != 0) }, seq(100))
+Filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100))
+sum(Filter(function(i) { all(i %% c(3,5,7) != 0) }, seq(100)))
 
 #10
- seq.int(100,1)
+ 
+reverse_sequence <- (1:100)
+rev(reverse_sequence)
 
- length(seq.int(100,1))
+ length(reverse_sequence)
  
 #11
-multiples = c()
+natural_num<- Filter(function(i) { all(i %% 3==0|| i %% 5==0 )}, seq(24))
+natural_num
 
-for (i in seq(1,25)) {
-  if (i%%3 == 0 || i%%5 == 0) {
-    multiples = c(multiples, i)
-  }
-}
-sum(multiples)
 
-length(multiples)
 #11a
 
-sum(length(multiples),length(seq_integer))
+
 
 #12
  x <- 0+ x + 5 +
@@ -71,11 +77,16 @@ sum(length(multiples),length(seq_integer))
 
 #13
  score <-c(72,86,89,92,63,88,89,91,92,75,75,77)
+ element10 <- score[10]
+  element11 <- score[11]
+  
+  element10
+  element11
  
 #14
  
  a <- c(1,2,NA,4,NA,6,7)
- print(a,na.print = 999)
+ print(a,na.print ="999")
  
 
 #15
@@ -84,7 +95,8 @@ sum(length(multiples),length(seq_integer))
  age =readline(prompt = "19")
  print(paste("My name is",name,"and I am",age,"years old"))
  print(R.version.string)
- 
+ My_Name <-c ("Janessa Marie Esmalla")
+ My_Name
 
 
 
