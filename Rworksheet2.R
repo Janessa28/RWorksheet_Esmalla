9f64f17fe38bc7046708e8cdf13f29808e869383
Janessa Marie Esmalla <janessaesmalla@gmail.com>
2023-09-28 04:06:01 +0000

    jamjam

---
+++ Rworksheet2.R
@@ -0,0 +1,27 @@
+#1 Create a vector using : operator
+#a. Sequence from -5 to 5. Write the R code and its output.
+#Describe its output.
+
+
+
+
+#b. x <- 1:7. What will be the value of x?
+
+
+
+#2 Create a vector using seq() function
+#a. seq(1, 3, by=0.2) # specify step size
+#Write the R script and its output. Describe the output.
+
+
+
+#3Create a vector using seq() function
+#a. seq(1, 3, by=0.2) # specify step size
+#Write the R script and its output. Describe the output.
+
+
+
+
+#Create a vector using seq() function
+#a. seq(1, 3, by=0.2) # specify step size
+#Write the R script and its output. Describe the output.
